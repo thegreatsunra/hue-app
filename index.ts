07064a59cb20e8fcd684ac06bd90d6e65efd6ef2
Dane Petersen <thegreatsunra@gmail.com>
2021-08-25 22:06:06 -0500

    Wrap app in a function

--- index.ts
+++ index.ts
@@ -1,25 +1,27 @@
 import { v3 } from "node-hue-api";
 import dotenv from "dotenv";
 
-dotenv.config();
+const app = () => {
+	const USERNAME = process.env["USERNAME"] || "";
+	// The name of the light we wish to retrieve by name
+	const LIGHT_ID = process.env["LIGHT_ID"] || "";
 
-const USERNAME = process.env["USERNAME"] || "";
-// The name of the light we wish to retrieve by name
-const LIGHT_ID = process.env["LIGHT_ID"] || "";
+	dotenv.config();
 
-const { LightState } = v3.lightStates;
+	const { LightState } = v3.lightStates;
 
-v3.discovery
-	.nupnpSearch()
-	.then((searchResults) => {
-		const host = searchResults[0].ipaddress;
-		return v3.api.createLocal(host).connect(USERNAME);
-	})
-	.then((api) => {
-		// Using a LightState object to build the desired state
-		const state = new LightState().on().brightness(100);
-		return api.lights.setLightState(LIGHT_ID, state);
-	})
-	.then((result) => {
-		console.log(`Light state change was successful? ${result}`);
-	});
+	v3.discovery
+		.nupnpSearch()
+		.then((searchResults) => {
+			const host = searchResults[0].ipaddress;
+			return v3.api.createLocal(host).connect(USERNAME);
+		})
+		.then((api) => {
+			// Using a LightState object to build the desired state
+			const state = new LightState().on().brightness(100);
+			return api.lights.setLightState(LIGHT_ID, state);
+		})
+		.then((result) => {
+			console.log(`Light state change was successful? ${result}`);
+		});
+app();
